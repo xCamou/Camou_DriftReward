75d699ee7d3cefa14f112a59134da9ab6fa0828b
xCamou <167819732+xCamou@users.noreply.github.com>
2025-12-12 17:40:49 +0100

    Refactor config.lua formatting

--- config.lua
+++ config.lua
@@ -1,10 +1,8 @@
 Config = {}
 
--- Basis Multiplikator (Geld = Score * Multiplier)
 Config.RewardMultiplier = 0.5
 
--- Mindestscore, ab dem Belohnung gezahlt wird
 Config.MinScore = 150
 
--- Optional: Rundung auf ganze Zahlen
 Config.RoundRewards = true
+
